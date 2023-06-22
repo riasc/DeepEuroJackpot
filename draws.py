c2a2b09b5c48767ed29ab5f6f28602d53eceb6e8
riasc <richard.schaefer@zoho.com>
2023-06-21 23:48:35 -0500

    tab delimiter missing in file and refactoring

--- draws.py
+++ draws.py
@@ -2,27 +2,41 @@ import os
 import pandas as pd
 
 class Draws:
-    data = [] 
+    data = {
+        "date": [], "1": [], "2": [], "3": [],
+        "4": [],"5": [], "E1": [], "E2": []
+    }
 
     def __init__(self, path):
         numbers = sorted(os.listdir(path))  # files containing the draws per year
-        for draws in numbers:
-            self.parse_draws(os.path.join(path,draws))
+        self.parse_draws(numbers)
+
+        # for draws in numbers:
+            # self.parse_draws(os.path.join(path,draws))
+        # df = pd.DataFrame(self.data)
+        # df.index = df['date']
+        # del df['date']
+        # print(df)
 
     # parse the draws within each year
-    def parse_draws(self,path):
-        data = {
-            "date": [],
-            "1": [],
-            "2": [],
-            "3": [],
-            "4": [],
-            "5": [],
-            "E1": [],
-            "E2": []
-        }
-        with open(path, "r") as draws:
-            for draw in draws:
-                no = draw.rstrip().split('\t')
+    def parse_draws(self, draws):
+        for year in draws:
+
+
+            with open(os.path.join(, "r") as draws:
+                next(draws)  # ignore header 
+                for draw in draws:
+                    no = draw.rstrip().split('\t')
+                    for i,v in enumerate(self.data.keys()):
+                        self.data[v].append(no[i])
+
+
+
+
+
+
+
+#        print(data)
+
 
 
