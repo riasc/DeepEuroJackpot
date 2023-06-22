12577508e5170e34c09f7231da8521fdea42d4ac
riasc <richard.schaefer@zoho.com>
2023-06-22 00:02:56 -0500

    resolved error in data frame when loading the data

--- draws.py
+++ draws.py
@@ -2,41 +2,45 @@ import os
 import pandas as pd
 
 class Draws:
-    data = {
-        "date": [], "1": [], "2": [], "3": [],
-        "4": [],"5": [], "E1": [], "E2": []
-    }
+
+    draws = pd.DataFrame() 
 
     def __init__(self, path):
-        numbers = sorted(os.listdir(path))  # files containing the draws per year
-        self.parse_draws(numbers)
+        years = []
+        for year in sorted(os.listdir(path)):
+            years.append(os.path.join(path, year))
 
-        # for draws in numbers:
-            # self.parse_draws(os.path.join(path,draws))
-        # df = pd.DataFrame(self.data)
-        # df.index = df['date']
-        # del df['date']
-        # print(df)
+        # create data frame from data
+        self.draws = pd.DataFrame(self.parse_draws(years))
+        self.draws.index = self.draws['date']
+        del self.draws['date']
+
+        print(self.draws)
+
 
     # parse the draws within each year
-    def parse_draws(self, draws):
-        for year in draws:
-
-
-            with open(os.path.join(, "r") as draws:
+    def parse_draws(self, years):
+        data = {
+            "date": [], 
+            "1": [], 
+            "2": [], 
+            "3": [],
+            "4": [],
+            "5": [], 
+            "E1": [], 
+            "E2": []
+        }
+        for year in years:
+            with open(year,'r') as draws:
                 next(draws)  # ignore header 
                 for draw in draws:
                     no = draw.rstrip().split('\t')
-                    for i,v in enumerate(self.data.keys()):
-                        self.data[v].append(no[i])
+                    for i,v in enumerate(data.keys()):
+                        data[v].append(no[i])
+
+        return data
 
 
-
-
-
-
-
-#        print(data)
 
 
 
